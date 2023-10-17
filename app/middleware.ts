ddc1bc4cc6980fb61c3504b50642a1521429808e
YassonDC <yassonword@gmail.com>
2023-10-17 17:38:26 -0500

    middleware update

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 // "/api/degrees/:path*"
-export const config = { matcher: ["https://teachercontentmanager.netlify.app/dashboard/:path*"] };
+export const config = { matcher: ["/dashboard/:path*"] };
