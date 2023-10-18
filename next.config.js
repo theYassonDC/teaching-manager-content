d22565d6b801800e99c75dce3cefc7b2093efb1f
YassonDC <yassonword@gmail.com>
2023-10-17 19:32:30 -0500

    next.config set `reactStrictMode = true`

--- next.config.js
+++ next.config.js
@@ -3,13 +3,11 @@
  */
 const nextConfig = {
   // output: 'export',
- 
+  reactStrictMode: true,
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
- 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
- 
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'build',
 }
