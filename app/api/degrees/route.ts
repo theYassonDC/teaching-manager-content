00a2d5f757f0ba2f96d64e51f8c4656c3e5b7367
YassonDC <yassonword@gmail.com>
2023-10-19 16:47:56 -0500

    update route api

--- app/api/degrees/route.ts
+++ app/api/degrees/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
-    const degrees = await prisma.degree.findFirst()
+    const degrees = await prisma.degree.findMany()
     return NextResponse.json({ degrees }, {status: 200})
   } catch (error) {
     console.log(error)
