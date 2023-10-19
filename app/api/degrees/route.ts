2dbcd52705a84c0dc46c6eaf00290ec1a8d9d5ef
YassonDC <yassonword@gmail.com>
2023-10-19 18:08:10 -0500

    revalidate routes

--- app/api/degrees/route.ts
+++ app/api/degrees/route.ts
@@ -1,13 +1,10 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from 'next/server'
 import { prisma } from "@/libs";
-import { revalidatePath } from 'next/cache'
 
 export async function GET(request: NextRequest) {
   try {
-    const path: any = request.nextUrl.searchParams.get('path')
     const degrees = await prisma.degree.findMany()
-    revalidatePath(path)
     return NextResponse.json({ degrees }, {status: 200})
   } catch (error) {
     console.log(error)
