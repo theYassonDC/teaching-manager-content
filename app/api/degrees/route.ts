e12d555368a3b0ff1fb1ae003acf84fe3e9af856
YassonDC <yassonword@gmail.com>
2023-10-19 16:31:50 -0500

    `findFirst` added

--- app/api/degrees/route.ts
+++ app/api/degrees/route.ts
@@ -1,8 +1,9 @@
 import { prisma } from "@/libs";
 import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
   try {
-    const degrees = await prisma.degree.findMany({ include: { topics: true }})
+    const degrees = await prisma.degree.findFirst()
     return NextResponse.json({ degrees }, {status: 200})
   } catch (error) {
     console.log(error)
