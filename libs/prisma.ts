ce37a3a3f4d68b3fa9fadbef6ee210cf4b000468
YassonDC <yassonword@gmail.com>
2023-10-23 15:45:59 -0500

    prisma client new client

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 declare global {
 	var prisma: PrismaClient | undefined;
 }
-// export const prisma = new PrismaClient() || global.prisma; 
-export const prisma = global.prisma ?? new PrismaClient();
+// export const prisma = global.prisma ?? new PrismaClient();
+export const prisma = new PrismaClient() || global.prisma; 
 if (process.env.NODE !== "production") global.prisma = prisma
 
