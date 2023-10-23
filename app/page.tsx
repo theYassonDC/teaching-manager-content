8693f446259bbe888f239302881d4185959b5d0d
YassonDC <yassonword@gmail.com>
2023-10-23 15:37:35 -0500

    update prisma libs and configure disable cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-wrap items-center flex-col gap-5">
       <h1 className='text-gray-50 text-3xl p-5 mt-24'>Gestion de contenido escolar</h1>
-        <Button href='/login' color="primary" radius='md' size='lg' as={Link}>
-          Iniciar session
-        </Button>
-        <Button href='/register' color="success" radius='md' size='lg' as={Link}>
-          Registrarme
-        </Button>
+      <Button href='/login' color="primary" radius='md' size='lg' as={Link}>
+        Iniciar session
+      </Button>
+      <Button href='/register' color="success" radius='md' size='lg' as={Link}>
+        Registrarme
+      </Button>
     </main>
   )
 }
