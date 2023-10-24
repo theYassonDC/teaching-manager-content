9d8a7cab4b9bc836fa2606fb153c877500d25980
YassonDC <yassonword@gmail.com>
2023-10-24 15:36:12 -0500

    dios mi ayuda este error vale verga 0034

--- app/api/degrees/route.ts
+++ app/api/degrees/route.ts
@@ -1,12 +1,21 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs";
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
     const degrees = await prisma.degree.findFirst()
     return NextResponse.json({ degrees }, {status: 200})
   } catch (error) {
     console.log(error)
-    NextResponse.json({ error: 'Not found' }, {status: 404})
+    return NextResponse.json({ error: 'Not found' }, {status: 404})
   }
 }
+
+// export async function getStaticProps () {
+//   return {
+//     props: {
+//       degrees // esto pasa los registros de ingresos como props al componente
+//     },
+//     revalidate: 10 // esto indica que la página se regenerará cada 10 segundos en el servidor
+//   }
+// }
