f45e1daffd97eda810fd957347f3fe852589e0e8
YassonDC <yassonword@gmail.com>
2023-10-24 15:42:18 -0500

    function fist page

--- app/api/degrees/route.ts
+++ app/api/degrees/route.ts
@@ -1,18 +1,17 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs";
 
+async function getDegrees(): Promise<any[]> {
+  const degrees = await prisma.degree.findMany()
+  return degrees
+}
+
 export async function GET(request: Request) {
   try {
-    const degrees = await prisma.degree.findFirst()
-    return NextResponse.json({ degrees }, {status: 200})
+    const list = await getDegrees()
+    return NextResponse.json({ list }, {status: 200})
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: 'Not found' }, {status: 404})
-  }
-}
-
-export async function getStaticProps () {
-  return {
-    revalidate: 0 // esto indica que la página se regenerará cada 10 segundos en el servidor
   }
 }
