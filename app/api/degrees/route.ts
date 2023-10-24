8e3b5b0c60a5e976b1b37a97584b8359a54aa2e7
YassonDC <yassonword@gmail.com>
2023-10-24 14:55:51 -0500

    dfdsfds

--- app/api/degrees/route.ts
+++ app/api/degrees/route.ts
@@ -1,13 +1,13 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { prisma } from "@/libs";
 import { GetServerSidePropsContext } from "next";
 
 export async function GET(request: Request, context: GetServerSidePropsContext) {
   try {
     const degrees = await prisma.degree.findMany()
-    return Response.json({ degrees }, {status: 200})
+    return NextResponse.json({ degrees }, {status: 200})
   } catch (error) {
     console.log(error)
-    Response.json({ error: 'Not found' }, {status: 404})
+    NextResponse.json({ error: 'Not found' }, {status: 404})
   }
 }
