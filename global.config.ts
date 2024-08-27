9aebecf63bf6f0b176d10ca3c0a73e670747f6b4
YassonDC <yassonword@gmail.com>
2024-08-27 02:45:46 -0500

    Stage mode

--- global.config.ts
+++ global.config.ts
@@ -1,6 +1,8 @@
 const globalConfig = {
-    baseUrl: 'https://teaching-manager-content.vercel.app',
-    cookies: '__Secure-next-auth.session-token'
+    baseUrl: 'https://teaching-manager-content.vercel.app', // --> Production
+    cookies: '__Secure-next-auth.session-token' // --> Production
+    // baseUrl: 'http://localhost:3000', // --> Dev
+    // cookies: 'next-auth.session-token' // --> Dev
 }
 
 export default globalConfig
